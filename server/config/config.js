99ef6cc136a797889c47b9a5101575a8ced75fa2
Carlos Aravena Arriagada <caravenaarriagada@gmail.com>
2020-05-18 19:07:17 -0400

    REST Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,15 @@
 // Get Port
 process.env.PORT = process.env.PORT || 3000;
+
+// Environment
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Connect DB
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://CharlyGood:q45yG9pS5GG5Ymet@cluster0-6fx0x.mongodb.net/cafe';
+}
+
+process.env.URL_DB = urlDB;
