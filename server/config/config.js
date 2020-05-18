994f2541e97de44d9541194387a53bb94230dbf0
Carlos Aravena Arriagada <caravenaarriagada@gmail.com>
2020-05-18 19:24:44 -0400

    Variables de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://CharlyGood:q45yG9pS5GG5Ymet@cluster0-6fx0x.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
